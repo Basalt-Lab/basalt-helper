f1be674bf82efd9091f463933cfca6a2e35ed8c8
Ruby <necrelox@proton.me>
2023-11-02 10:47:04 +0100

    docs: update readme, and add JSdoc

--- Sources/Data/Transformations/BasaltKebabCaseTransformer.ts
+++ Sources/Data/Transformations/BasaltKebabCaseTransformer.ts
@@ -1,6 +1,27 @@
 import { IBasaltKeyTransformer } from '@/Interfaces';
 
+/**
+ * Transforms string keys into kebab-case format.
+ * Implements the IBasaltKeyTransformer interface for key transformation.
+ */
 export class BasaltKebabCaseTransformer implements IBasaltKeyTransformer {
+
+    /**
+     * Transforms a single key from any case to kebab-case.
+     *
+     * @param {string} key - The key string to transform into kebab-case.
+     * @returns {string} - The key string transformed into kebab-case, with all letters in lower case and words separated by hyphens.
+     *
+     * @example
+     * // returns "my-key-name"
+     * transformKey('myKeyName');
+     * @example
+     * // returns "my-key-name"
+     * transformKey('MyKeyName');
+     * @example
+     * // returns "my-long-key-name"
+     * transformKey('myLongKeyName');
+     */
     public transformKey(key: string): string {
         return key
             .replace(/([a-z])([A-Z])/g, '$1-$2')
--- Sources/Data/Transformations/BasaltPascalCaseTransformer.ts
+++ Sources/Data/Transformations/BasaltPascalCaseTransformer.ts
@@ -1,6 +1,27 @@
 import { IBasaltKeyTransformer } from '@/Interfaces';
 
+/**
+ * Transforms string keys into PascalCase format.
+ * Implements the IBasaltKeyTransformer interface for key transformation.
+ */
 export class BasaltPascalCaseTransformer implements IBasaltKeyTransformer {
+
+    /**
+     * Transforms a single key from any case to PascalCase.
+     *
+     * @param {string} key - The key string to transform into PascalCase.
+     * @returns {string} - The key string transformed into PascalCase, with the first letter of each word capitalized.
+     *
+     * @example
+     * // returns "MyKeyName"
+     * transformKey('my_key_name');
+     * @example
+     * // returns "MyKeyName"
+     * transformKey('my-key-name');
+     * @example
+     * // returns "MyLongKeyName"
+     * transformKey('myLongKeyName');
+     */
     public transformKey(key: string): string {
         const camelCaseKey: string = key.replace(/([-_][a-z])/gi, (group: string) =>
             group[1].toUpperCase()

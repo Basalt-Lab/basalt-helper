8a952a34b97dfc9aea76b0220ee02f14f01b5f16
ruby <necrelox@proton.me>
2024-03-26 17:52:16 +0100

    fix: remove useless code ...

--- Source/App.ts
+++ Source/App.ts
@@ -2,10 +2,3 @@ export * from './Common/Errors';
 export * from './Domain/Services/Data';
 export * from './Domain/Services/Data/TransformerStrategies';
 export * from './Domain/Services/Security';
-
-import { hashPassword } from './Domain/Services/Security';
-
-(async (): Promise<void> => {
-    const hashedPassword = await hashPassword('password');
-    console.log(hashedPassword);
-})();

03162d4c460162a72a7ee8edf5e8bf978660ba1c
ruby <necrelox@proton.me>
2024-10-02 11:45:59 +0200

    feat: remove useless utils

--- source/common/util/index.ts
+++ source/common/util/index.ts
@@ -1,6 +1,2 @@
-export * from './dirname.util.ts';
-export * from './file.util.ts';
-export * from './folder.util.ts';
 export * from './hash.util.ts';
-export * from './path.util.ts';
 export * from './sleep.util.ts';

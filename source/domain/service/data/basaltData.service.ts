08f58e276faaeb4520514f75c97138df8b2bdd87
ruby <necrelox@proton.me>
2024-12-17 10:46:59 +0100

    refactor(🧹): refacto Error Systems
    ## Refactor:
    Major updates to error handling 👻✨
    
    ### Key changes:
    - 🔑 `ErrorKeys` has been renamed to `GLOBAL_ERRORS` and its structure has been updated:
      From: `key: string -> value: string` (where the value was just a potential translation key)
      To: `key: string -> value: [string, number]` (now a tuple containing a translation key and an HTTP status code).
      This change groups the HTTP status code and the i18n key together, eliminating the need to specify the code separately each time `BasaltError` is used.
    
    - 💼 All global constants are now in **`SCREAMING_SNAKE_CASE`** 😱 (applies to `BasaltError` and `BasaltToken`).
    
    - 🎯 `BasaltError` now only accepts two optional parameters: `key` and `cause`.
    
    These improvements make error handling more streamlined and consistent across the project 🚀.

--- source/domain/service/data/basaltData.service.ts
+++ source/domain/service/data/basaltData.service.ts
@@ -1,37 +1,71 @@
-import { BasaltError } from '#/common/error/basaltError.ts';
-import { ErrorKeys } from '#/common/error/errorKeys.ts';
+import { BasaltError } from '#/common/error/basalt.error.ts';
+import { GLOBAL_ERRORS } from '#/common/error/global.error.ts';
 import type { BasaltKeyTransformer } from '#/common/type/data/basaltKeyTransformer.ts';
 
 /**
+ * Checks if the provided data is null or undefined and throws an error if it is.
+ *
+ * @typeParam T - The type of the data to be validated.
+ *
+ * @param data - The data to be validated.
+ *
+ * @throws ({@link BasaltError}) - Throws an error if the data is null or undefined. ({@link GLOBAL_ERRORS.DATA_IS_NULL})
+ */
+function _validateDataNull<T>(data: T): void {
+    if (data === null || data === undefined)
+        throw new BasaltError({
+            key: GLOBAL_ERRORS.DATA_IS_NULL
+        });
+}
+
+/**
+ * Checks if the provided data is an object and throws an error if it is not.
+ *
+ * @typeParam T - The type of the data to be validated.
+ *
+ * @param data - The data to be validated.
+ *
+ * @throws ({@link BasaltError}) - Throws an error if the data is not a plain object. ({@link GLOBAL_ERRORS.DATA_MUST_BE_OBJECT})
+ */
+function _validateDataIsObject<T>(data: T): void {
+    if (typeof data !== 'object')
+        throw new BasaltError({
+            key: GLOBAL_ERRORS.DATA_MUST_BE_OBJECT
+        });
+}
+
+/**
  * Creates a deep clone of the provided data object.
+ *
+ * @deprecated This method is deprecated and will be removed in the future. Use the `structuredClone` method instead.
+ * @see https://nodejs.org/api/globals.html#structuredclonevalue-options
  *
  * @typeParam T - The type of the data to be cloned.
  *
  * @param data - The data object to be cloned.
  *
- * @throws ({@link BasaltError}) - Throws an error if the data is null or undefined. ({@link ErrorKeys.DATA_IS_NULL})
+ * @throws ({@link BasaltError}) - Throws an error if the data is null or undefined. ({@link GLOBAL_ERRORS.DATA_IS_NULL})
  *
  * @returns The deep cloned object. ({@link T})
  */
 export function deepClone<T>(data: T): T {
-    if (data === null || data === undefined)
-        throw new BasaltError({
-            messageKey: ErrorKeys.DATA_IS_NULL
-        });
+    _validateDataNull(data);
     if (data instanceof Date)
         return new Date(data.getTime()) as T;
     if (data instanceof RegExp)
         return new RegExp(data) as T;
     if (Array.isArray(data))
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
         return data.map((item: unknown) => deepClone(item)) as T;
     if (typeof data === 'object') {
         const clonedObject: Partial<T> = {} as Partial<T>;
         for (const key in data)
             if (Object.hasOwn(data, key))
+                // eslint-disable-next-line @typescript-eslint/no-deprecated
                 clonedObject[key as keyof T] = deepClone(data[key]) as T[keyof T];
         return clonedObject as T;
     }
-    return data as T;
+    return data;
 }
 
 /**
@@ -46,8 +80,8 @@ export function deepClone<T>(data: T): T {
  * @param keys - The array of keys to exclude from the data object. (Can be empty)
  * @param excludeNullUndefined - Flag to determine if properties with null or undefined values should be excluded.
  *
- * @throws ({@link BasaltError}) - Throws an error if the data is null or undefined. ({@link ErrorKeys.DATA_IS_NULL})
- * @throws ({@link BasaltError}) - Throws an error if the data is not a plain object. ({@link ErrorKeys.DATA_MUST_BE_PLAIN_OBJECT})
+ * @throws ({@link BasaltError}) - Throws an error if the data is null or undefined. ({@link GLOBAL_ERRORS.DATA_IS_NULL})
+ * @throws ({@link BasaltError}) - Throws an error if the data is not a plain object. ({@link GLOBAL_ERRORS.DATA_MUST_BE_OBJECT})
  *
  * @example
  * ```typescript
@@ -66,14 +100,8 @@ export function deepClone<T>(data: T): T {
  * @returns The filtered data object with the specified keys excluded. ({@link T})
  */
 export function filterByKeyExclusion<T extends Readonly<object>>(data: Readonly<T>, keys: readonly (keyof T)[], excludeNullUndefined = false): T {
-    if (data === null || data === undefined)
-        throw new BasaltError({
-            messageKey: ErrorKeys.DATA_IS_NULL
-        });
-    if (typeof data !== 'object')
-        throw new BasaltError({
-            messageKey: ErrorKeys.DATA_MUST_BE_PLAIN_OBJECT
-        });
+    _validateDataNull(data);
+    _validateDataIsObject(data);
     const filteredData: T = {} as T;
     Object.keys(data).forEach((key: string): void => {
         const typedKey: keyof T = key as keyof T;
@@ -93,8 +121,8 @@ export function filterByKeyExclusion<T extends Readonly<object>>(data: Readonly<
  * @param keys - The array of keys to include in the resulting data object. (Can be empty)
  * @param excludeNullUndefined - Flag to determine if properties with null or undefined values should be excluded.
  *
- * @throws ({@link BasaltError}) - Throws an error if the data is null or undefined. ({@link ErrorKeys.DATA_IS_NULL})
- * @throws ({@link BasaltError}) - Throws an error if the data is not a plain object. ({@link ErrorKeys.DATA_MUST_BE_PLAIN_OBJECT})
+ * @throws ({@link BasaltError}) - Throws an error if the data is null or undefined. ({@link GLOBAL_ERRORS.DATA_IS_NULL})
+ * @throws ({@link BasaltError}) - Throws an error if the data is not a plain object. ({@link GLOBAL_ERRORS.DATA_MUST_BE_OBJECT})
  *
  * @example
  * ```typescript
@@ -113,14 +141,8 @@ export function filterByKeyExclusion<T extends Readonly<object>>(data: Readonly<
  * @returns The filtered data object with only the specified keys included. ({@link T})
  */
 export function filterByKeyInclusion<T extends Readonly<object>>(data: Readonly<T>, keys: readonly (keyof T)[], excludeNullUndefined = false): T {
-    if (data === null || data === undefined)
-        throw new BasaltError({
-            messageKey: ErrorKeys.DATA_IS_NULL
-        });
-    if (typeof data !== 'object')
-        throw new BasaltError({
-            messageKey: ErrorKeys.DATA_MUST_BE_PLAIN_OBJECT
-        });
+    _validateDataNull(data);
+    _validateDataIsObject(data);
     const filteredData: T = {} as T;
     keys.forEach((key: keyof T): void => {
         if (key in data && (!excludeNullUndefined || (data[key] !== null && data[key] !== undefined)))
@@ -139,8 +161,8 @@ export function filterByKeyInclusion<T extends Readonly<object>>(data: Readonly<
  * @param predicate - The predicate function to apply to the values.
  * @param excludeNullUndefined - Flag to determine if properties with null or undefined values should be excluded. Default is false.
  *
- * @throws ({@link BasaltError}) - Throws an error if the data is null or undefined. ({@link ErrorKeys.DATA_IS_NULL})
- * @throws ({@link BasaltError}) - Throws an error if the data is not a plain object. ({@link ErrorKeys.DATA_MUST_BE_PLAIN_OBJECT})
+ * @throws ({@link BasaltError}) - Throws an error if the data is null or undefined. ({@link GLOBAL_ERRORS.DATA_IS_NULL})
+ * @throws ({@link BasaltError}) - Throws an error if the data is not a plain object. ({@link GLOBAL_ERRORS.DATA_MUST_BE_OBJECT})
  *
  * @example
  * ```typescript
@@ -159,15 +181,8 @@ export function filterByKeyInclusion<T extends Readonly<object>>(data: Readonly<
  * @returns The filtered data object with properties satisfying the predicate. ({@link T})
  */
 export function filterByValue<T extends Readonly<object>> (data: Readonly<T>, predicate: (value: T[keyof T]) => boolean, excludeNullUndefined = false): T {
-    if (data === null || data === undefined)
-        throw new BasaltError({
-            messageKey: ErrorKeys.DATA_IS_NULL
-        });
-    if (typeof data !== 'object')
-        throw new BasaltError({
-            messageKey: ErrorKeys.DATA_MUST_BE_PLAIN_OBJECT
-        });
-
+    _validateDataNull(data);
+    _validateDataIsObject(data);
     const filteredData: T = {} as T;
     for (const key in data)
         if (Object.hasOwn(data, key)) {
@@ -184,24 +199,20 @@ export function filterByValue<T extends Readonly<object>> (data: Readonly<T>, pr
  * @param data - The object whose keys are to be transformed.
  * @param transformer - The key transformation strategy to use.
  *
- * @throws ({@link BasaltError}) - If the provided data object is null or undefined. ({@link ErrorKeys.DATA_IS_NULL})
- * @throws ({@link BasaltError}) - If the provided data object is not a plain object. ({@link ErrorKeys.DATA_MUST_BE_PLAIN_OBJECT})
+ * @throws ({@link BasaltError}) - If the provided data object is null or undefined. ({@link GLOBAL_ERRORS.DATA_IS_NULL})
+ * @throws ({@link BasaltError}) - If the provided data object is not a plain object. ({@link GLOBAL_ERRORS.DATA_MUST_BE_OBJECT})
+*
+ * @example
+ * ```typescript
+ * // Return { myKey: "value" }
+ * transformKeys(\{ "my-key": "value" \}, new BasaltCamelCaseTransformer());
+ * ```
  *
  * @returns A new object with transformed keys. ({@link T})
- *
- * @example
- * Returns \{ myKey: "value" \}
- * transformKeys(\{ "my-key": "value" \}, new BasaltCamelCaseTransformer());
  */
 export function transformKeys<T extends Readonly<object>>(data: Readonly<T>, transformer: Readonly<BasaltKeyTransformer>): T {
-    if (data === null || data === undefined)
-        throw new BasaltError({
-            messageKey: ErrorKeys.DATA_IS_NULL
-        });
-    if (typeof data !== 'object')
-        throw new BasaltError({
-            messageKey: ErrorKeys.DATA_MUST_BE_PLAIN_OBJECT
-        });
+    _validateDataNull(data);
+    _validateDataIsObject(data);
     const result: T = {} as T;
 
     for (const key in data)

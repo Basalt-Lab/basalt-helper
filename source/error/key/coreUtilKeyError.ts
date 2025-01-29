d476b621f3df42341518f7ca257249a63787cbbb
ruby <necrelox@proton.me>
2025-01-29 17:31:13 +0100

    refactor(🧹): update error message keys for basalt-helper localization

--- source/error/key/coreUtilKeyError.ts
+++ source/error/key/coreUtilKeyError.ts
@@ -6,10 +6,10 @@ export const CORE_UTIL_KEY_ERROR = {
      * Interpolation :
      * - name: The name of the class.
      */
-    CLASS_CONSTRUCTOR_ALREADY_REGISTERED: ['error.basalt-helper.class_constructor_already_registered', 500],
+    CLASS_CONSTRUCTOR_ALREADY_REGISTERED: ['basalt-helper.error.class_constructor_already_registered', 500],
     /**
      * Interpolation :
      * - name: The name of the class.
      */
-    CLASS_CONSTRUCTOR_NOT_REGISTERED: ['error.basalt-helper.class_constructor_not_registered', 500]
+    CLASS_CONSTRUCTOR_NOT_REGISTERED: ['basalt-helper.error.class_constructor_not_registered', 500]
 } as const;
